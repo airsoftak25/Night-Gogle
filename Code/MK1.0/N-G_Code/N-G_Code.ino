8f73f11b91895da3a9c6cd69f04f7f9a10f443ae
Airsofták <vadimsob25@gmail.com>
2025-06-04 22:49:22 +0200

    gg

--- Code/MK1.0/N-G_Code/N-G_Code.ino
+++ Code/MK1.0/N-G_Code/N-G_Code.ino
@@ -1,128 +1,135 @@
 #include "esp_camera.h"
 #include <TFT_eSPI.h>
 #include <SPI.h>
+#include <Arduino.h>
 
-#define IR_LED_PIN 15      // Výstup pro MOSFET IR LED
-#define BUTTON_PIN 0       // Tlačítko GND → aktivní LOW
-#define TFT_BL 4           // Podsvícení displeje (PWM)
+#define TFT_BL      4       // PWM pro podsvit displeje
+#define BUTTON_PIN  14      // Tlačítko
+#define IR_LED_PIN  12      // Výstup pro IR LED (MOSFET)
+#define CAM_LED_PIN 4       // GPIO vestavěné LED na ESP32-CAM
 
 TFT_eSPI tft = TFT_eSPI();
+int brightness = 128;
+bool fadeUp = true;
+unsigned long lastFade = 0;
 
-bool irLedState = false;
-bool dimming = false;
-
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 50;
-
-int brightness = 255;  // 0–255
-int fadeDirection = -5; // Směr stmívání (− stmívá, + rozjasňuje)
-unsigned long lastFadeTime = 0;
-const unsigned long fadeInterval = 30;
-
-void setupCamera() {
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = 5;
-  config.pin_d1 = 18;
-  config.pin_d2 = 19;
-  config.pin_d3 = 21;
-  config.pin_d4 = 36;
-  config.pin_d5 = 39;
-  config.pin_d6 = 34;
-  config.pin_d7 = 35;
-  config.pin_xclk = 0;
-  config.pin_pclk = 22;
-  config.pin_vsync = 25;
-  config.pin_href = 23;
-  config.pin_sscb_sda = 26;
-  config.pin_sscb_scl = 27;
-  config.pin_pwdn = 32;
-  config.pin_reset = -1;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_RGB565;
-
-  if (psramFound()) {
-    config.frame_size = FRAMESIZE_QQVGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_QQVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
-
-  if (esp_camera_init(&config) != ESP_OK) {
-    Serial.println("Kamera selhala");
-  }
-}
-
-void setupPWM() {
-  ledcSetup(1, 5000, 8);        // Channel 1, 5kHz, 8bit
-  ledcAttachPin(TFT_BL, 1);     // BLK pin na PWM
-  ledcWrite(1, brightness);     // Start brightness
-}
+bool buttonHeld = false;
+bool irState = false;
+unsigned long pressStart = 0;
+bool buttonPressed = false;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(IR_LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  // Vypnutí vestavěné LEDky na ESP32-CAM (GPIO 4)
+  pinMode(CAM_LED_PIN, OUTPUT);
+  digitalWrite(CAM_LED_PIN, LOW);
 
-  setupPWM();
-  digitalWrite(IR_LED_PIN, LOW);
-
+  // Inicializace displeje
   tft.init();
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.drawString("Inicializace...", 20, 120);
 
-  setupCamera();
+  // PWM pro podsvit
+  ledcSetup(1, 5000, 8);
+  ledcAttachPin(TFT_BL, 1);
+  ledcWrite(1, brightness);
+
+  // Inicializace tlačítka a IR LED
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(IR_LED_PIN, OUTPUT);
+  digitalWrite(IR_LED_PIN, LOW);
+
+  // Inicializace kamery
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0       = 5;
+  config.pin_d1       = 18;
+  config.pin_d2       = 19;
+  config.pin_d3       = 21;
+  config.pin_d4       = 36;
+  config.pin_d5       = 39;
+  config.pin_d6       = 34;
+  config.pin_d7       = 35;
+  config.pin_xclk     = 0;
+  config.pin_pclk     = 22;
+  config.pin_vsync    = 25;
+  config.pin_href     = 23;
+  config.pin_sscb_sda = 26;
+  config.pin_sscb_scl = 27;
+  config.pin_pwdn     = 32;
+  config.pin_reset    = -1;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_RGB565;
+  config.frame_size   = FRAMESIZE_QVGA;
+  config.fb_count     = 1;
+
+  if (esp_camera_init(&config) != ESP_OK) {
+    tft.fillScreen(TFT_RED);
+    tft.drawString("KAMERA CHYBA!", 20, 120);
+    while (true);
+  }
+
+  tft.fillScreen(TFT_BLACK);
+  tft.drawString("Hotovo", 80, 120);
+  delay(500);
+  tft.fillScreen(TFT_BLACK);
 }
 
 void loop() {
-  static bool lastButtonState = HIGH;
-  bool reading = digitalRead(BUTTON_PIN);
+  handleButton();
+  displayCameraFrame();
+}
 
-  // Detekce hran
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
+void handleButton() {
+  bool pressed = digitalRead(BUTTON_PIN) == LOW;
+
+  if (pressed && !buttonHeld) {
+    pressStart = millis();
+    buttonHeld = true;
+    buttonPressed = false;
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // Stisk – krátký (toggle IR LED)
-    if (reading == LOW && lastButtonState == HIGH && !dimming) {
-      irLedState = !irLedState;
-      digitalWrite(IR_LED_PIN, irLedState ? HIGH : LOW);
-    }
-
-    // Držení tlačítka – aktivuje stmívání
-    if (reading == LOW) {
-      dimming = true;
-    } else {
-      dimming = false;
+  if (pressed && buttonHeld && millis() - pressStart > 400) {
+    buttonPressed = true;
+    if (millis() - lastFade > 25) {
+      brightness += (fadeUp ? 5 : -5);
+      brightness = constrain(brightness, 0, 255);
+      fadeUp = (brightness >= 255) ? false : (brightness <= 0) ? true : fadeUp;
+      ledcWrite(1, brightness);
+      lastFade = millis();
     }
   }
-  lastButtonState = reading;
 
-  // Stmívání, pokud držím tlačítko
-  if (dimming && millis() - lastFadeTime > fadeInterval) {
-    brightness += fadeDirection;
-    if (brightness <= 30 || brightness >= 255) {
-      fadeDirection = -fadeDirection;
-      brightness = constrain(brightness, 30, 255);
+  if (!pressed && buttonHeld) {
+    buttonHeld = false;
+    if (!buttonPressed && millis() - pressStart < 400) {
+      irState = !irState;
+      digitalWrite(IR_LED_PIN, irState ? HIGH : LOW);
     }
-    ledcWrite(1, brightness);
-    lastFadeTime = millis();
   }
+}
 
-  // Živý obraz
+void displayCameraFrame() {
   camera_fb_t *fb = esp_camera_fb_get();
   if (!fb) {
-    Serial.println("Chyba snímku");
+    Serial.println("Chyba: žádný snímek");
     return;
   }
 
-  // Převedení RGB565 obrazu do displeje
-  tft.pushImage(0, 0, fb->width, fb->height, (uint16_t *)fb->buf);
+  // Pokud se z kamery dostane RGB565 (např. při přenastavení výstupu), zobraz přímo
+  if (fb->format == PIXFORMAT_RGB565) {
+    tft.pushImage(0, 0, 240, 240, (uint16_t *)fb->buf);
+  } else {
+    // Pokud ne, zobraz informaci
+    tft.fillScreen(TFT_RED);
+    tft.setTextColor(TFT_WHITE, TFT_RED);
+    tft.drawString("Nespravny format!", 10, 120);
+  }
+
   esp_camera_fb_return(fb);
 }
